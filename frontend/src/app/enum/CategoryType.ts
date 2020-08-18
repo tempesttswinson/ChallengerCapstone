a72200538f6a7abe0c3f2ebc7b45aea2e6b55651
tempesttswinson <tempestt.swinson@gmail.com>
2020-08-18 11:11:48 -0400

    added margin tops to each page

--- frontend/src/app/enum/CategoryType.ts
+++ frontend/src/app/enum/CategoryType.ts
@@ -1,3 +1,3 @@
 export enum CategoryType {
-    "Books", "Food", "Clothes", "Drink"
+    "Fruits", "Veggies"
 }
--- frontend/src/app/pages/home/home.component.ts
+++ frontend/src/app/pages/home/home.component.ts
@@ -25,7 +25,6 @@ export class HomeComponent implements OnInit {
       });
     });
 
-    // $('.carousel').carousel();
 
   }
 
